e555eec80322a44f9b63231d7fc6eb8098822e70
Zach <46905452+whitakerz@users.noreply.github.com>
2025-03-02 13:43:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ if uploaded_file is not None:
     retriever = vector.as_retriever(search_type="similarity", search_kwargs={"k": 5})
 
     # Define the LLM and the prompt
-    llm = Ollama(model="deepseek-v2:16b")
+    llm = Ollama(model="initium/law_model")
     prompt = """
     1. Use the following pieces of context to answer the question at the end.
     2. If you don't know the answer, just say that "I don't know" but don't make up an answer on your own.\n
